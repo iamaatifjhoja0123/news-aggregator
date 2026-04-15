7d5ef76c8ae5176bf239414ee439d4cb7361fc4c
Aatif Jhoja <m.aatif0123@gmail.com>
2026-04-15 17:17:28 +0530

    Enabled HTTPS with Let's Encrypt and Nginx proxy

--- infra/main.tf
+++ infra/main.tf
@@ -63,6 +63,13 @@ resource "aws_security_group" "docker_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     from_port   = 5000
     to_port     = 5000
